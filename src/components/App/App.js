ed7a3ddf2d3d21935649b6331efc1d6671720076
Eric Leite <ericxleite@gmail.com>
2018-03-25 17:57:56 -0700

    Initial project setup

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+// Libs
+import React, { Component } from 'react';
+
+// CSS
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>Hello world!</div>
+    );
+  }
+
+}
+
+export default App;
